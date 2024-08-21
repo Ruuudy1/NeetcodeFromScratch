0b330f3f2b7ed2b20a8bb11dd70ed447341213f6
Rudy Osuna <130013367+Ruuudy1@users.noreply.github.com>
2024-08-20 20:41:09 -0700

    Update 0242-valid-anagram.py
    first solution

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,3 +3,14 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
+
+        # sSet = sorted(s)
+        # tSet = sorted(t)
+
+        # if len(s) is not len(t):
+        #     return False
+            
+        # for i in range(len(s)):
+        #     if sSet[i] is not tSet[i]:
+        #         return False
+        # return True
