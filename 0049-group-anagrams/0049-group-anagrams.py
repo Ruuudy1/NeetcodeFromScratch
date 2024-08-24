9f31e77d45ab8dfd45e0d5603d3ede68cd479477
Rudy Osuna <130013367+Ruuudy1@users.noreply.github.com>
2024-08-23 22:43:13 -0700

    Time: 89 ms (60.44%), Space: 19.7 MB (74.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,15 @@
-from collections import Counter 
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = {}
+        anagram_dict = defaultdict(list)
+        
         for s in strs:
-            count = Counter(s)
-            key = tuple(sorted(count.items()))
-            if key in res:
-                res[key].append(s)
-            else:
-                res[key] = [s]
-        return list(res.values())
+            # Sort the string and use it as the key
+            key = ''.join(sorted(s))
+            # Append the original string to the list corresponding to this sorted key
+            anagram_dict[key].append(s)
+        
+        # Return the grouped anagrams as a list of lists
+        return list(anagram_dict.values())
