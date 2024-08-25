66dad0634d49f1f2c8a4ca717128e744b2177611
Rudy Osuna <130013367+Ruuudy1@users.noreply.github.com>
2024-08-24 23:38:52 -0700

    Time: 87 ms (66.82%), Space: 21.1 MB (66.11%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [x[0] for x in Counter(nums).most_common(k)]
+        # return [x[0] for x in Counter(nums).most_common(k)]
 
-        # tupleList = list(Counter(nums).most_common(k))
-        # return [x[0] for x in tupleList]
+        tupleList = list(Counter(nums).most_common(k))
+        return [x[0] for x in tupleList]
