72225910d10cfa04c28f2a8d8d63aa8ff17c3f87
Rudy Osuna <130013367+Ruuudy1@users.noreply.github.com>
2024-12-20 22:33:43 -0800

    Update 0104-maximum-depth-of-binary-tree.py
    added comments

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -5,12 +5,19 @@
 #         self.left = left
 #         self.right = right
 
+
+#RECURSIVE DFS
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
-            return 0
+            return 0 
+        # we know there is at least 1 node since the root is not empty 
+        # meaning we will return 1 + the max dfs of the left side and right side
 
         countR = self.maxDepth(root.right)
         countL = self.maxDepth(root.left)
 
+        # this +1 is what makes the whole problem work, since every new function call, 
+        # the left child turns into the root of the new subproblem adding one once again 
+        # and then searching for its children
         return 1 + max(countL, countR)
